7d56f9238f2bb224a490a55547da8b1f7f214c2a
laita <laita.kamila32@smk.belajar.id>
2026-04-29 08:36:34 +0700

    commit2

--- index.php
+++ index.php
@@ -7,7 +7,59 @@ include 'koneksi.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>data pengguna</title>
-    <link rel="stylesheet" href="style.css">
+    <style>
+        body{
+            font-family: 'Poppins', sans-serif;
+            background-color: white;
+            margin: 0;
+        }
+
+        .container{
+            width: 90%;
+            max-width: 900px;
+            margin: 40px auto;
+            background: white;
+            padding: 25px;
+            border-radius: 15px;
+            box-shadow: 0 8px 20px rgba(0,0,0,0.1);
+        }
+
+        h1{
+            text-align: center;
+            color: #2e7d32;
+        }
+
+        .btn{ 
+            padding:10px 15px; 
+            background:#2e7d32; 
+            color:white; 
+            text-decoration:none; 
+            border-radius:8px; 
+            display:inline-block; 
+            margin-bottom:5px;   
+        }
+
+        table{
+            width:100%;
+            border-collapse:collapse;
+            background:white;
+            border-radius:10px;
+            overflow:hidden;
+            margin-top: 20px;
+        }
+
+        th{
+            background:#2e7d32;
+            color:white;
+            padding:12px;
+        }
+
+        td{
+            padding:12px;
+            text-align:center;
+        }
+
+    </style>
 </head>
 <body>
     <div class="container">
